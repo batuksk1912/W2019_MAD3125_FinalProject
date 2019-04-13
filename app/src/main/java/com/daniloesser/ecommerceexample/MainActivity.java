4fe7e1210fbd6ae60b7b21d3918baf15500b3045
Danilo Esser <daniloesser@gmail.com>
2019-04-12 20:21:12 -0400

    First Commit

---
+++ app/src/main/java/com/daniloesser/ecommerceexample/MainActivity.java
@@ -0,0 +1,27 @@
+package com.daniloesser.ecommerceexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        joinNowButton = findViewById(R.id.main_join_now_btn);
+        loginButton = findViewById(R.id.main_login_btn);
+
+
+        loginButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
