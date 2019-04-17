9feafa9281989d8f2740df51dce3d26f246cd6b8
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-04-17 05:18:07 -0400

    Admin can see orders. Start from 31.

--- app/src/main/java/com/daniloesser/ecommerceexample/AdminCategoryActivity.java
+++ app/src/main/java/com/daniloesser/ecommerceexample/AdminCategoryActivity.java
@@ -3,17 +3,42 @@ package com.daniloesser.ecommerceexample;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminCategoryActivity extends AppCompatActivity {
 
     private ImageView tShirts, sportsTShirts, femaleDresses, sweathers, glasses, hatsCaps, walletsBagsPurses, shoes, headphonesHandFree, laptops, watches, mobilePhones;
+    private Button logoutButton, checkOrdersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_category);
+
+        logoutButton = findViewById(R.id.admin_logout_btn);
+        checkOrdersButton = findViewById(R.id.check_orders_btn);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        checkOrdersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         tShirts = findViewById(R.id.t_shirts);
         sportsTShirts = findViewById(R.id.sports_t_shirts);
